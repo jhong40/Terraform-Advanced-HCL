f7c42a8e4ae72b25b060ebc2ac2529bc1d53ffed
ned1313 <ned.bellavance@gmail.com>
2025-11-03 08:48:44 -0500

    base app is good and we have the first solution

---
+++ solutions/m1_solution/main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Data source to get available AZs
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Local values for common tags
+locals {
+  common_tags = {
+    Environment = var.environment
+    ManagedBy   = var.team
+    Owner       = coalesce(var.owner, var.team)
+  }
+  name_prefix = lower(format("%s-%s", var.team, var.environment))
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-vpc", local.name_prefix)
+  })
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 0)
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-public1", local.name_prefix)
+  })
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 1)
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-public2", local.name_prefix)
+  })
+}
+
+resource "aws_subnet" "public_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 2)
+  availability_zone = data.aws_availability_zones.available.names[2]
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-public3", local.name_prefix)
+  })
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(local.common_tags, {
+    Name = format("%s-igw", local.name_prefix)
+  })
+
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = format("%s-rtb", local.name_prefix)
+  })
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_3" {
+  subnet_id      = aws_subnet.public_3.id
+  route_table_id = aws_route_table.public.id
+}
